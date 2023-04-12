2a320d1f14226074512dc8338941337a55c262d8
sparowell <103679474+sparowell@users.noreply.github.com>
2023-04-12 21:08:46 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy.stats import cramervonmises_2samp
 
 chat_id = 654929803 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
+    #ks_2samp
     p = cramervonmises_2samp(x, y).pvalue # Это как раз CVM
     return p <= 0.01 
