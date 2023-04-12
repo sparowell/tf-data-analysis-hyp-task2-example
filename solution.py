f157ab010b472990995c2d6a120e2fc5f7a92436
sparowell <103679474+sparowell@users.noreply.github.com>
2023-04-12 21:43:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import cramervonmises_2samp
+from hyppo.ksample import MMD
 
 chat_id = 654929803 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     #ks_2samp
-    p = cramervonmises_2samp(x, y).pvalue # Это как раз CVM
+    #p = cramervonmises_2samp(x, y).pvalue # Это как раз CVM
+    p = MMD(compute_kernel="rbf", gamma=10).test(x, y)[1]
     return p <= 0.01 
